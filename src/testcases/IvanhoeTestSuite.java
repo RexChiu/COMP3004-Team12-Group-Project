57dc77739eb13196b0c5b75a306e9c634e5120bc
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-14 23:48:01 -0500

    Set up the Junit Test and initialized all basic unit test

---
+++ src/testcases/IvanhoeTestSuite.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TestIvanhoe.class,
+   TestIvanhoeRequests.class,
+   TestIvanhoeResponses.class
+})
+
+public class IvanhoeTestSuite {
+
+}
