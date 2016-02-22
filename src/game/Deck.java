bff315ee8d4a5326778f1352ff4e70cedc9ae52d
Rex Chiu <rex.chiu@outlook.com>
2016-02-22 18:06:40 -0500

    Deck: Implemented clone

--- src/game/Deck.java
+++ src/game/Deck.java
@@ -9,12 +9,12 @@ import config.GAMEConfig;
 public class Deck {
 
 	private List<Card> deck;
-	
+
 	public Deck() {
 		// TODO Auto-generated constructor stub
 		this.deck = new ArrayList<Card>();
 	}
-	
+
 	public void 	addCard(Card card)		{ this.deck.add(card); 				}
 	public boolean 	removeCard(Card card)	{ return this.deck.remove(card); 	}
 	public Card 	getCard(int index)		{ return this.deck.get(index);		}
@@ -22,7 +22,14 @@ public class Deck {
 	public void 	shuffleDeck()			{ Collections.shuffle(this.deck);	}
 	public boolean 	isEmpty()				{ return this.deck.isEmpty(); 		}
 	public void 	cleanDeck()				{ this.deck.clear(); 				}
-	
+
+	// Clone deep copy the deck
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Deck cloned = (Deck)super.clone();
+		return cloned;
+	}
+
 	public boolean equals (Deck other)
 	{
 		if (this.getSize() != other.getSize())
@@ -36,7 +43,7 @@ public class Deck {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 }
