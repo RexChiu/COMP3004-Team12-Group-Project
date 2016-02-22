183a927e5d9b9f21e9ab9c392b8df84f0203c55c
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-21 23:44:06 -0500

    Display: Implement the method changeWeapon(String tournament)

--- src/game/Display.java
+++ src/game/Display.java
@@ -38,6 +38,9 @@ public class Display {
 
 	//UNHORSE: The tournament color changes from purple to red, blue or yellow
 	public void 	unhorse(String tournament) 		{ this.tournament = tournament; }
+	
+	//CHANGE WEAPON: The tournament color changes from red, blue or yellow to a differ
+	public void 	changeWeapon(String tournament) { this.tournament = tournament; }
 
 	// Check the green tournament
 	public boolean 	isGreenTournament() { return this.tournament.equalsIgnoreCase(GAMEConfig.COLOR_GREEN); 		}
