10b1679c6b68757162d53ee3c75d452258ffc744
Rex Chiu <rex.chiu@outlook.com>
2016-02-22 17:35:38 -0500

    Deck: Implemented addCard

---
+++ src/game/Deck.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import config.GAMEConfig;
+
+public class Deck {
+
+	private List<Card> deck;
+	
+	public Deck() {
+		// TODO Auto-generated constructor stub
+		this.deck = new ArrayList<Card>();
+	}
+	
+	public void 	addCard(Card card)		{ this.deck.add(card); 				}
+}
