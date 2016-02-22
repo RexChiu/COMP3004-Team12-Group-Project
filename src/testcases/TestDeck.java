8b5c2ed56f0543088876eaa01dd7da5fbff823f7
Rex Chiu <rex.chiu@outlook.com>
2016-02-22 17:33:12 -0500

    TestDeck: testAddCard test

---
+++ src/testcases/TestDeck.java
@@ -0,0 +1,49 @@
+package testcases;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import game.Card;
+import game.Deck;
+
+public class TestDeck {
+	Deck deck;
+	
+    @BeforeClass
+    public static void BeforeClass() {
+        System.out.println("@BeforeClass: TestDeck");
+    }
+    
+    @Before
+    public void setUp() {
+		System.out.println("@Before: TestDeck");
+		deck = new Deck();
+	}
+	
+    @After
+    public void tearDown () {
+		System.out.println("@After(): TestDeck");
+	}
+	
+    @AfterClass
+    public static void afterClass () {
+    	 System.out.println("@AfterClass: TestDeck");
+    }
+    
+    @Test
+	public void testAddCard () 
+    {
+		System.out.println("@Test(): addCard(Card card)");
+		
+		Card card = new Card("Ivanhoe", "Action Card", 0);
+		
+		deck.addCard(card);
+				
+		assertEquals(card, deck.getCard(0));
+    }
+}
