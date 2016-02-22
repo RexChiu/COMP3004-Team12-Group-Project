87480718a5103041406680155200edee72251fec
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-21 20:31:58 -0500

    Create Source code for Tokens and Token

---
+++ src/game/Token.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Token {
+
+	private String name;
+	
+	public Token(String name) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+	}
+ 
+	public String 	getName() 	{ return this.name; 	}
+}
---
+++ src/game/Tokens.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.util.HashMap;
+
+public class Tokens {
+	
+	private HashMap <String, Token> tokens;
+	
+	public Tokens(String token) {
+		// TODO Auto-generated constructor stub
+		tokens = new HashMap<String, Token>();
+		tokens.put(token, new Token(token));
+	}
+	
+}
