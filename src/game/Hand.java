e359dd37eb63de724fd6861fd69c5c0801576404
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-21 21:27:43 -0500

    Create Source code for Hand and Card

---
+++ src/game/Hand.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.util.ArrayList;
+
+import config.GAMEConfig;
+
+public class Hand {
+
+	private ArrayList<Card> hand;
+	
+	public Hand() {
+		// TODO Auto-generated constructor stub
+		hand = new ArrayList<Card>();
+	}
+}
