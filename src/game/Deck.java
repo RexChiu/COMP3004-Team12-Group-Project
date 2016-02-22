de002a04560b350a132017eafc082552489492bb
Rex Chiu <rex.chiu@outlook.com>
2016-02-22 17:42:15 -0500

    Deck: Implemented getSize

--- src/game/Deck.java
+++ src/game/Deck.java
@@ -19,4 +19,5 @@ public class Deck {
 	public Card 	getCard(int index)		{ return this.deck.get(index);		}
 	public boolean 	removeCard(Card card)	{ return this.deck.remove(card); 	}
 	public boolean 	isEmpty()				{ return this.deck.isEmpty(); 		}
+	public int 		getSize()				{ return this.deck.size();			}
 }
