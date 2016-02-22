0802cb74e1a74ddae655a4af956f23c7c77af1c0
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-21 22:07:29 -0500

    Card: Implement the medtho isStunned()

--- src/game/Card.java
+++ src/game/Card.java
@@ -37,4 +37,5 @@ public class Card implements Cloneable{
 	// Special Card of Action Card
 	public boolean 	isIvanhoe()			{ return this.name.equalsIgnoreCase(GAMEConfig.IVANHOE);		}
 	public boolean 	isShield()			{ return this.name.equalsIgnoreCase(GAMEConfig.SHIELD);			}
+	public boolean 	isStunned()			{ return this.name.equalsIgnoreCase(GAMEConfig.STUNNED);		}
 }
