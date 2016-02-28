6111a7a1a98a4edff3cc346c1bd4fee74b2a539e
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-27 19:21:32 -0500

    Modify GUI TournamentPanel and make it public

--- src/gui/TournamentPanel.java
+++ src/gui/TournamentPanel.java
@@ -1,6 +1,8 @@
 package gui;
 
-import java.awt.*; 
+import java.awt.*;
+import java.util.HashMap;
+
 import javax.swing.*;
 
 import config.GUIConfig; 
@@ -12,24 +14,29 @@ public class TournamentPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = -6487157126971866036L;
 
+	public HashMap<Integer, JButton> infoLabel 		= new HashMap<Integer, JButton>();
+	public HashMap<Integer, JButton> statusLabel	= new HashMap<Integer, JButton>();
+	public HashMap<Integer, JButton> totalLabel 	= new HashMap<Integer, JButton>();
+	
+	
 	public TournamentPanel() { 
 		setLayout(null);
 
-		for (int i = 0; i < 5; i ++){
-			JButton infoLabel = new JButton("INFO");
-			infoLabel.setLocation(75*i, 0);
-			infoLabel.setSize(GUIConfig.TOURNAMENT_INFO_WIDTH, GUIConfig.TOURNAMENT_INFO_HEIGHT);
-			add(infoLabel);
+		for (int i = GUIConfig.USER_PLAYER_ID; i <= GUIConfig.FIFTH_PLAYER_ID; i ++){
+			infoLabel.put(i, new JButton("INFO"));
+			infoLabel.get(i).setLocation(75*i, 0);
+			infoLabel.get(i).setSize(GUIConfig.TOURNAMENT_INFO_WIDTH, GUIConfig.TOURNAMENT_INFO_HEIGHT);
+			add(infoLabel.get(i));
 			
-			JButton statusLabel = new JButton("Status");
-			statusLabel.setLocation(75*i, 100);
-			statusLabel.setSize(GUIConfig.TOURNAMENT_STATUS_WIDTH, GUIConfig.TOURNAMENT_STATUS_HEIGHT);
-			add(statusLabel);
+			statusLabel.put(i, new JButton("Status"));
+			statusLabel.get(i).setLocation(75*i, 100);
+			statusLabel.get(i).setSize(GUIConfig.TOURNAMENT_STATUS_WIDTH, GUIConfig.TOURNAMENT_STATUS_HEIGHT);
+			add(statusLabel.get(i));
 			
-			JButton totalLabel = new JButton("Total");
-			totalLabel.setLocation(75*i, 150);
-			totalLabel.setSize(GUIConfig.TOURNAMENT_TOTAL_WIDTH, GUIConfig.TOURNAMENT_TOTAL_HEIGHT);
-			add(totalLabel);
+			totalLabel.put(i, new JButton("Total"));
+			totalLabel.get(i).setLocation(75*i, 150);
+			totalLabel.get(i).setSize(GUIConfig.TOURNAMENT_TOTAL_WIDTH, GUIConfig.TOURNAMENT_TOTAL_HEIGHT);
+			add(totalLabel.get(i));
 		}
 
 		setLocation(GUIConfig.TOURNAMENT_PANEL_LOCATION_X, GUIConfig.TOURNAMENT_PANEL_LOCATION_Y);
