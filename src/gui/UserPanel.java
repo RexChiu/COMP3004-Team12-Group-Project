20bdcea269b47d9844f15d4aa8b597c874798d2d
Hector Zhang <hectorzhang2010@hotmail.com>
2016-02-27 19:22:51 -0500

    Modify GUI UserPanel and make it public

--- src/gui/UserPanel.java
+++ src/gui/UserPanel.java
@@ -11,43 +11,45 @@ public class UserPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = -449738241414461419L;
 
+	public JButton tokenButton, infoButton, statusOneButton, statusTwoButton, totalButton, displayButton, handButton;
+	
 	public UserPanel() { 
 		setLocation(GUIConfig.USER_PANEL_LOCATION_X, GUIConfig.USER_PANEL_LOCATION_Y);
 		setSize(GUIConfig.USER_PANEL_WIDTH, GUIConfig.USER_PANEL_HEIGHT);
 		setLayout(null);	
 		
-		JButton tokenButton = new JButton("Token");
+		tokenButton = new JButton("Token");
 		tokenButton.setLocation(GUIConfig.USER_TOKEN_LOCATION_X, GUIConfig.USER_TOKEN_LOCATION_Y);
 		tokenButton.setSize(GUIConfig.USER_TOKEN_WIDTH, GUIConfig.USER_TOKEN_HEIGHT);
 		add(tokenButton);
 		
-		JButton infoButton = new JButton("Info");
+		infoButton = new JButton("Info");
 		infoButton.setLocation(GUIConfig.USER_INFO_LOCATION_X, GUIConfig.USER_INFO_LOCATION_Y);
 		infoButton.setSize(GUIConfig.USER_INFO_WEIGTH, GUIConfig.USER_INFO_HEIGHT);
 		add(infoButton);
 		
-		JButton handButton = new JButton("Hand");
+		handButton = new JButton("Hand");
 		handButton.setLocation(GUIConfig.USER_HAND_LOCATION_X, GUIConfig.USER_HAND_LOCATION_Y);
 		handButton.setSize(GUIConfig.USER_HAND_WIDTH, GUIConfig.USER_HAND_HEIGHT);
 		add(handButton);
 		
 		
-		JButton statusOneButton = new JButton("One");
+		statusOneButton = new JButton("One");
 		statusOneButton.setLocation(GUIConfig.USER_STATUS_ONE_LOCATION_X, GUIConfig.USER_STATUS_ONE_LOCATION_Y);
 		statusOneButton.setSize(GUIConfig.USER_STATUS_WIDTH, GUIConfig.USER_STATUS_HEIGHT);
 		add(statusOneButton);
 		
-		JButton statusTwoButton = new JButton("Two");
+		statusTwoButton = new JButton("Two");
 		statusTwoButton.setLocation(GUIConfig.USER_STATUS_TWO_LOCATION_X, GUIConfig.USER_STATUS_TWO_LOCATION_Y);
 		statusTwoButton.setSize(GUIConfig.USER_STATUS_WIDTH, GUIConfig.USER_STATUS_HEIGHT);
 		add(statusTwoButton);
 		
-		JButton totalButton = new JButton("Total");
+		totalButton = new JButton("Total");
 		totalButton.setLocation(GUIConfig.USER_TOTAL_LOCATION_X, GUIConfig.USER_TOTAL_LOCATION_Y);
 		totalButton.setSize(GUIConfig.USER_TOTAL_SIZE, GUIConfig.USER_TOTAL_SIZE);
 		add(totalButton);
 		
-		JButton displayButton = new JButton("Dispaly");
+		displayButton = new JButton("Dispaly");
 		displayButton.setLocation(GUIConfig.USER_DISPLAY_LOCATION_X, GUIConfig.USER_DISPLAY_LOCATION_Y);
 		displayButton.setSize(GUIConfig.USER_DISPLAY_WIDTH, GUIConfig.USER_DISPLAY_HEIGHT);
 		add(displayButton);
